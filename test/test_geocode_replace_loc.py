1c25b260b3b08377140ebca89c9f9d816d5127e9
Jill Rabinowitz <jillrabinowitz@gmail.com>
2013-03-13 11:19:46 -0700

    Added asserts to verify database values for particular xml file being processed

--- test/test_geocode_replace_loc.py
+++ test/test_geocode_replace_loc.py
@@ -28,107 +28,127 @@ class TestGeocodeReplaceLoc(unittest.TestCase):
         query = domestic_sql() % (-1, -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
-        #print rows
+        element = rows[0][2]
+        self.assertTrue('FAIRHAVEN'==element, "{0} should be {1}".format(element,'FAIRHAVEN'))
         query = domestic_sql() % (0, 0)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
-        #print rows
+        element = rows[3][2]
+        self.assertTrue('MOUNT KISCO'==element, "{0} should be {1}".format(element,'MOUNT KISCO'))
         pass
 
     def test_domestic_block_remove_sql(self):
         query = domestic_block_remove_sql() % (-1, -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
+        element = rows[4][2]
+        self.assertTrue('BOCA RATON'==element, "{0} should be {1}".format(element,'BOCA RATON'))
         pass
 
     def test_domestic_first3_jaro_winkler_sql(self):
         query = domestic_first3_jaro_winkler_sql() % (-1, -1, "10.92", -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
+        element = rows[6][6]
+        self.assertTrue('LAGUNA HILLS'==element,"{0} should be {1}".format(element,'LAGUNA HILLS'))
         pass
 
     def test_domestic_last4_jaro_winkler_sql(self):
         query = domestic_last4_jaro_winkler_sql() % (-1, -1, "10.90", -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
+        element = rows[8][4]
+        self.assertTrue('US'==element,"{0} should be {1}".format(element,'US'))
         pass
 
     def test_foreign_full_name_1_sql(self):
         query = foreign_full_name_1_sql() % (-1, -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows, "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_full_name_2_sql(self):
         query = foreign_full_name_2_sql() % (-1, -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows, "{0} should be {1}".format(num_rows,-1))    
         pass
 
     def test_foreign_short_form_sql(self):
         query = foreign_short_form_sql() % (-1, -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows , "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_block_split_sql(self):
         query = foreign_block_split_sql() % (-1, -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows, "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_first3_jaro_winkler_sql(self):
         query = foreign_first3_jaro_winkler_sql() % (-1, -1, "20.92", -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows= result.rowcount
+        self.assertEquals(-1, num_rows, "{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_last4_jaro_winkler_sql(self):
         query = foreign_last4_jaro_winkler_sql() % (-1, -1, "20.90", -1)
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_domestic_2nd_layer_sql(self):
         query = domestic_2nd_layer_sql()
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_domestic_first3_2nd_jaro_winkler_sql(self):
         query = domestic_first3_2nd_jaro_winkler_sql() % "14.95"
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_full_name_2nd_layer_sql(self):
         query = foreign_full_name_2nd_layer_sql()
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_full_nd_2nd_layer_sql(self):
         query = foreign_full_nd_2nd_layer_sql()
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_no_space_2nd_layer_sql(self):
         query = foreign_no_space_2nd_layer_sql()
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_first3_2nd_jaro_winkler_sql(self):
         query = foreign_first3_2nd_jaro_winkler_sql() % "24.95"
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_domestic_zipcode_sql(self):
         query = domestic_zipcode_sql()
         result = self.cursor.execute(query)
-        rows = result.fetchall()
+        num_rows = result.rowcount
+        self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
 
--- test/test_geocode_setup.py
+++ test/test_geocode_setup.py
@@ -28,28 +28,39 @@ class TestGeocodeSetup(unittest.TestCase):
     def test_fix_city_country(self):
         make_assignee_db()
         fix_city_country(self.c)
-        # Inspect loc table in hashTbl, find something
-        # to assert.
-        rows = self.c.execute('select * from loc').fetchall()
-        print rows[0][1]
+        # Inspect loc table in hashTbl, find something to assert.
+        rows = self.c.execute('select * from loc order by city').fetchall()
         element = rows[0][1]
-        assert('FRISCO' == element)
+        self.assertTrue('BERLIN'==element,"{0} should be {1}".format(element,'BERLIN'))
+        element = rows[0][3]
+        self.assertTrue('DE' == element,"{0} should be {1}".format(element,'DE'))
+        element = rows[2][1]
+        self.assertTrue('FAIRHAVEN' == element,"{0} should be {1}".format(element,'FAIRHAVEN'))
+        element = rows[2][2]
+        self.assertTrue('MA' == element,"{0} should be {1}".format(element,'MA'))
         pass
 
     def test_fix_state_zip(self):
         make_inventor_db()
         fix_state_zip(self.c)
-        # Inspect loc table in hashTbl, find something
-        # to assert.
-        rows = self.c.execute('select * from loc').fetchall()
-        #print rows
+        # Inspect loc table in hashTbl, find something to assert.
+        rows = self.c.execute('select * from loc order by city').fetchall()
+        element = rows[3][1]
+        self.assertTrue('FRISCO' == element,"{0} should be {1}".format(element,'FRISCO'))
+        element = rows[0][5]
+        self.assertTrue('BER' == element,"{0} should be {1}".format(element,'BER'))
+        element = rows[6][1]
+        self.assertTrue('LAGUNA HILLS' == element,"{0} should be {1}".format(element, 'LAGUNA HILLS'))
         pass
 
     def test_create_usloc_table(self):
         create_loc_indexes(self.conn)
         create_usloc_table(self.c)
-        # Inpect the usloc table, find some assertion to make.
-        assert('FOO' == 'FOO')
+        rows = self.c.execute('select * from usloc').fetchall()
+        element = rows[0][0]
+        self.assertTrue(92274 == element, "{0} should be {1}".format(element,92274))
+        element = rows[3][3]
+        self.assertTrue('115 FIRMS' == element,"{0} should be {1}".format(element, '115 FIRMS'))
 
     def tearDown(self):
         pass
