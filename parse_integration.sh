8d83aff96269d2f19ef46a164ce225ab00cb6932
Gabe Fierro <gtfierro225@gmail.com>
2013-06-06 10:33:29 -0700

    temporarily modify parse_integration

--- parse_integration.sh
+++ parse_integration.sh
@@ -11,28 +11,28 @@ mkdir -p tmp/integration/ipg120327.one
 for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
 do
   sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.one/${table}.csv
-  diff test/integration/parse/ipg120327.one/${table}.csv tmp/integration/ipg120327.one/${table}.csv
+  diff test/integration/new_parser/ipg120327.one/${table}.csv tmp/integration/ipg120327.one/${table}.csv
 done
 
 # TODO: Refactor
-make spotless > /dev/null
-mkdir -p tmp/integration/ipg120327.two
-./parse.py -d test/fixtures/xml/ -p. -x ipg120327.two.xml
-
-for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
-do
-  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.two/${table}.csv
-  diff test/integration/parse/ipg120327.two/${table}.csv tmp/integration/ipg120327.two/${table}.csv
-done
-
-make spotless > /dev/null
-mkdir -p tmp/integration/ipg120327.18
-./parse.py -d test/fixtures/xml/ -p. -x ipg120327.18.xml
-
-for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
-do
-  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.18/${table}.csv
-  diff test/integration/parse/ipg120327.18/${table}.csv tmp/integration/ipg120327.18/${table}.csv
-done
-
-
+#make spotless > /dev/null
+#mkdir -p tmp/integration/ipg120327.two
+#./parse.py -d test/fixtures/xml/ -p. -x ipg120327.two.xml
+#
+#for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+#do
+#  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.two/${table}.csv
+#  diff test/integration/parse/ipg120327.two/${table}.csv tmp/integration/ipg120327.two/${table}.csv
+#done
+#
+#make spotless > /dev/null
+#mkdir -p tmp/integration/ipg120327.18
+#./parse.py -d test/fixtures/xml/ -p. -x ipg120327.18.xml
+#
+#for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+#do
+#  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.18/${table}.csv
+#  diff test/integration/parse/ipg120327.18/${table}.csv tmp/integration/ipg120327.18/${table}.csv
+#done
+#
+#
