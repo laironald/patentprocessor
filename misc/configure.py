fc86bd6a7ff52b7b6e42b189b372415c4ea6212c
Gabe Fierro <gtfierro225@gmail.com>
2013-06-19 11:37:12 -0700

    move stuff around

--- misc/configure.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import yaml
-stream = file('test.yaml','r')
-configuration = yaml.load(stream)
-
-print configuration
-print configuration['filepath']
-print configuration['regex']
--- sql/invpat_index.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX idx_invpat_assignee on invpat (Assignee);
-CREATE INDEX idx_invpat_asgnum on invpat (Asgnum);           
-CREATE INDEX idx_invpat_gyear on invpat (Gyear);
-CREATE INDEX idx_invpat_invnum_n on invpat (Invnum_N);
-CREATE INDEX idx_invpat_city on invpat (City);
-CREATE INDEX idx_invpat_city_state on invpat (City, State);
-CREATE INDEX idx_invpat_state on invpat (State);
-CREATE INDEX idx_invpat_patent on invpat (Patent);
-CREATE INDEX idx_invpat_patent_invseq on invpat (Patent, Invseq);
