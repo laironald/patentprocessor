31be2d9ee60140aa3288b4a2f51929809a4ff288
Ron MacBook Air <laironald@gmail.com>
2013-06-25 15:51:41 -0500

    added new classes in alchemy

---
+++ lib/alchemy/README.md
@@ -0,0 +1,3 @@
+Functions
+  * [Adding Indices](http://stackoverflow.com/questions/6626810/multiple-columns-index-when-using-the-declarative-orm-extension-of-sqlalchemy)
+
--- lib/alchemy/__init__.py
+++ lib/alchemy/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Date, Integer, Float, String
-from sqlalchemy import Unicode, UnicodeText, ForeignKey, Text
+from sqlalchemy import Unicode, UnicodeText, ForeignKey, Index, Text
 from sqlalchemy.orm import backref, deferred, relationship
 from sqlalchemy.orm import sessionmaker, validates
 from sqlalchemy.ext.hybrid import hybrid_property
@@ -29,6 +29,16 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 session = Session()
 
+"""
+Here I am modifying the Declarative Base
+"""
+def init(self, *args, **kwargs):
+    for i, arg in enumerate(args):
+        self.__dict__[self.kw[i]] = arg
+    for k, v in kwargs.iteritems():
+        self.__dict__[k] = v
+Base.__init__ = init
+
 
 class Patent(Base):
     __tablename__ = "patent"
@@ -44,17 +54,36 @@ class Patent(Base):
     kind = Column(String(10))
     claims = Column(Integer)
 
+    __table_args__ = (
+        Index("pat_idx1", "grant__type", "grant__num", unique=True),
+        Index("pat_idx2", "app__type", "app__num", unique=True),
+        Index("pat_idx3", "date__grant"),
+        Index("pat_idx4", "date__app"),
+    )
+
     kw = ["grant__type", "grant__num", "date__grant",
           "app__type", "app__num", "date__app",
           "abstract", "title", "kind", "claims"]
-
-    def __init__(self, *args, **kwargs):
-        for i, arg in enumerate(args):
-            self.__dict__[self.kw[i]] = arg
-        for k, v in kwargs.iteritems():
-            self.__dict__[k] = v
+#Index('pat_idx3', Patent.date__grant)
 
 
+class USPC(Base):
+    __tablename__ = "uspc"
+    id = Column(String(20), primary_key=True, unique=True)
+    class_id = Column(String(10), ForeignKey("class.id"))
+    sequence = Column(Integer)
+    title = Column(String(256))
+    description = Column(String(256))
+
+
+class Class(Base):
+    __tablename__ = "class"
+    id = Column(String(20), primary_key=True, unique=True)
+    title = Column(String(256))
+    description = Column(String(256))
+
+
+"""
 class User(Base):
     __tablename__ = 'users'
 
@@ -80,14 +109,18 @@ class User(Base):
 #   deferred column loading
 #   deferred(Column(Text))
 #   read @ deferred data (for title, abstract and least)
+"""
 
 Base.metadata.create_all(engine)
+"""
 user = User('ed', 'mcedon', 'e@d')
-pat = Patent("D", "21321", "2005-12-12")
-pat.kind = "F"
-session.add(pat)
-session.add(Patent(date__grant="20040502", date__app="2004-2-2"))
-
 print user.fullname2
 session.add(user)
+"""
+pat = Patent("D", "21321")
+pat.kind = "F"
+session.add(pat)
+session.add(Patent(grant__num="22321"))
+#session.save_or_update(Patent(grant__num="22321", grant__date="20040205"))
+
 session.commit()
