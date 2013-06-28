e9fb79b73f99dc892031d06da3143162eeeba5eb
Ron MacBook Air <laironald@gmail.com>
2013-06-28 04:10:57 -0500

    added cluster

--- starcluster/fetch_xml.py
+++ starcluster/fetch_xml.py
@@ -10,11 +10,12 @@ print rc.ids
 @dview.remote(block=True)
 def fetch(year):
     import os
-    directory = "/mount/sgeadmin/{}".format(year)
+    directory = "/mnt/sgeadmin/{}".format(year)
     if not os.path.exists(directory):
         os.makedirs(directory)
     for f in files:
-        os.system("cd {0}; wget {1}; unzip {1]".format(directory, f))
+        fname = f.split("/")
+        os.system("cd {0}; wget {1}; unzip {2}".format(directory, f, fname))
 
 
 for year in xrange(2005, 2014):
