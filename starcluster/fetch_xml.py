ac3f5658e2fdbb455cfa46eca196b584c52648cb
Ron MacBook Air <laironald@gmail.com>
2013-06-28 15:51:23 -0500

    mv file to nfs server

--- starcluster/fetch_xml.py
+++ starcluster/fetch_xml.py
@@ -13,22 +13,26 @@ print rc.ids
 
 
 @dview.remote(block=True)
-def makedir(directory):
+def makedir(directory, base):
     import os
     try:
         os.makedirs(directory)
+        os.makedirs(base)
     except:
         pass
 
 @dview.remote(block=True)
-def fetch(directory):
+def fetch(directory, base):
     import os
     for f in files:
         fname = f.split("/")[-1]
         os.chdir(directory)
-        if not os.path.exists(fname):
-            os.system("wget {0}".format(f))
-            os.system("unzip {0}".format(fname))
+        if not os.path.exists("{0}/{1}".format(directory, fname)):
+            if not os.path.exists("{0}/{1}".format(base, fname)):
+                os.system("wget {0}".format(f))
+                os.system("mv {1} {0}/{1}".format(base, fname))
+            os.chdir(base)
+            os.system("unzip {1} -d {0}".format(directory, fname))
 
 
 fname = open("{0}/urls.pickle".format(config.get('directory', 'sqlalchemy')), "rb")
@@ -38,6 +42,7 @@ for year in xrange(2005, 2014):
     print year, datetime.now()
     dview.scatter("files", urls[year])
     directory = "{0}/{1}".format(config.get('directory', 'local'), year)
+    directory = "{0}/{1}".format(config.get('directory', 'xml'), year)
     print "  *", directory
     makedir(directory)
-    fetch(directory)
+    fetch(directory, base)
