d271947829cbde65849d925b3a80cff811205fca
Ron MacBook Air <laironald@gmail.com>
2013-07-02 14:28:13 -0500

    added a drop script

---
+++ starcluster/load_drop.sql
@@ -0,0 +1,13 @@
+drop table patent;
+drop table location;
+drop table subclass;
+drop table mainclass;
+drop table application;
+drop table assignee;
+drop table citation;
+drop table inventor;
+drop table ipcr;
+drop table lawyer;
+drop table otherreference;
+drop table usreldoc;
+drop table uspc;
--- starcluster/load_pre.sh
+++ starcluster/load_pre.sh
@@ -1,13 +1,17 @@
 #configured to my starcluster environment
 
 cd /mnt/sgeadmin
-for i in `ls *.xml` 
-    do echo $i 
+for i in `ls *.xml`
+    do echo $i
 
     cd /home/sgeadmin/patentprocessor/
-    python parse_sq.py -p /mnt/sgeadmin -xmlregex $i
+    echo "python"
+    python parse_sq.py -p /mnt/sgeadmin --xmlregex $i
+    echo "mysqldump"
     mysqldump -root uspto -T /var/lib/mysql/uspto
-    mysql -root uspto -e "drop database uspto; create database uspto;"
-    mysql [options] --local-infile=1 uspto < load.sql
+    echo "drop database"
+    mysql -root uspto < load_drop.sql
+    echo "ingest"
+    mysql [options] --local-infile=1 uspto < starcluster/load.sql
 
 done
