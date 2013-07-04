bcd0ad0bdf6ec3c02e5cca65ab06336bbaf52611
Ron MacBook Air <laironald@gmail.com>
2013-07-04 04:59:00 -0500

    reduced fetch

--- starcluster/fetch_xml.py
+++ starcluster/fetch_xml.py
@@ -18,25 +18,25 @@ def fetch(year):
     import glob
     import os
     ids = ids[0]
+    os.chdir(node)
     for i, f in enumerate(files):
         fname = f.split("/")[-1].split(".")[0]
-        os.chdir(node)
-        if not os.path.exists("{1}.xml".format(fname)):
+        if not os.path.exists("{0}.xml".format(fname)):
             os.system("wget {0}".format(f))
             os.system("unzip {0}.zip".format(fname))
-            os.chdir(master)
-            os.system("python parse_sq.py -p {0} --xmlregex {1} >> tar/{2}.log".format(node, fname, ids))
-            os.system("mysqldump -root uspto -T /var/lib/mysql/uspto")
-            os.chdir("/var/lib/mysql/uspto")
-            os.system("tar -czf {0}.tar.gz *.txt".format(fname))
-            for txts in glob.glob("*.txt"):
-                os.system("cat {0} >> {0}.full".format(txts))
-            os.system("rm *.txt")
-            os.system("scp {0}.tar.gz {1}/tar".format(fname, master))
+    #         os.chdir(master)
+    #         os.system("python parse_sq.py -p {0} --xmlregex {1} >> tar/{2}.log".format(node, fname, ids))
+    #         os.system("mysqldump -root uspto -T /var/lib/mysql/uspto")
+    #         os.chdir("/var/lib/mysql/uspto")
+    #         os.system("tar -czf {0}.tar.gz *.txt".format(fname))
+    #         for txts in glob.glob("*.txt"):
+    #             os.system("cat {0} >> {0}.full".format(txts))
+    #         os.system("rm *.txt")
+    #         os.system("scp {0}.tar.gz {1}/tar".format(fname, master))
 
-    os.chdir("/var/lib/mysql/uspto")
-    os.system("tar -czf {0}.tar.gz *.txt.full".format(ids))
-    os.system("mv {0}.tar.gz {1}/tar".format(ids, master))
+    # os.chdir("/var/lib/mysql/uspto")
+    # os.system("tar -czf {0}.tar.gz *.txt.full".format(ids))
+    # os.system("mv {0}.tar.gz {1}/tar".format(ids, master))
 
 
 fname = open("{0}/urls.pickle".format(config.get('directory', 'sqlalchemy')), "rb")
