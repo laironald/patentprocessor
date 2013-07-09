d7beca9eca332cf028de152aeb69b46230c04841
Kevin Johnson <vadskye@gmail.com>
2013-07-09 13:16:08 -0700

    Update test for new geocoding

--- test/test_geocode_replace_loc.py
+++ test/test_geocode_replace_loc.py
@@ -4,40 +4,38 @@ import sys
 import sqlite3
 import unittest
 
-sys.path.append( '.' )
-sys.path.append( '../lib/' )
+import make_test_databases
 
-from make_test_databases import *
-from geocode_setup import create_hashtbl
-from geocode_setup import create_sql_helper_functions
-from geocode_replace_loc import *
+sys.path.append('../')
+import lib.geocode_setup as geocode_setup
+import lib.geocode_replace_loc as geocode_replace_loc
 
 class TestGeocodeReplaceLoc(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        remove_existing_databases()
-        make_assignee_db()
-        make_inventor_db()
+        make_test_databases.remove_existing_databases()
+        make_test_databases.make_assignee_db()
+        make_test_databases.make_inventor_db()
         self.conn = sqlite3.connect("hashTbl.sqlite3")
         self.cursor = self.conn.cursor()
-        create_sql_helper_functions(self.conn)
-        create_hashtbl(self.cursor, self.conn)
+        geocode_setup.create_sql_helper_functions(self.conn)
+        geocode_setup.create_hashtbl(self.cursor, self.conn)
 
     def test_domestic_sql(self):
-        query = domestic_sql() % (-1, -1)
+        query = geocode_replace_loc.domestic_sql() % (-1, -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
         element = rows[0][2]
         self.assertTrue('FAIRHAVEN'==element, "{0} should be {1}".format(element,'FAIRHAVEN'))
-        query = domestic_sql() % (0, 0)
+        query = geocode_replace_loc.domestic_sql() % (0, 0)
         result = self.cursor.execute(query)
         element = rows[3][2]
         self.assertTrue('MOUNT KISCO'==element, "{0} should be {1}".format(element,'MOUNT KISCO'))
         pass
 
     def test_domestic_block_remove_sql(self):
-        query = domestic_block_remove_sql() % (-1, -1)
+        query = geocode_replace_loc.domestic_block_remove_sql() % (-1, -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
         element = rows[4][2]
@@ -45,7 +43,7 @@ class TestGeocodeReplaceLoc(unittest.TestCase):
         pass
 
     def test_domestic_first3_jaro_winkler_sql(self):
-        query = domestic_first3_jaro_winkler_sql() % (-1, -1, "10.92", -1)
+        query = geocode_replace_loc.domestic_first3_jaro_winkler_sql() % (-1, -1, geocode_setup.get_first3_jaro_required(), -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
         element = rows[6][6]
@@ -53,7 +51,7 @@ class TestGeocodeReplaceLoc(unittest.TestCase):
         pass
 
     def test_domestic_last4_jaro_winkler_sql(self):
-        query = domestic_last4_jaro_winkler_sql() % (-1, -1, "10.90", -1)
+        query = geocode_replace_loc.domestic_last4_jaro_winkler_sql() % (-1, -1, geocode_setup.get_last4_jaro_required(), -1)
         result = self.cursor.execute(query)
         rows = result.fetchall()
         element = rows[8][4]
@@ -61,91 +59,91 @@ class TestGeocodeReplaceLoc(unittest.TestCase):
         pass
 
     def test_foreign_full_name_1_sql(self):
-        query = foreign_full_name_1_sql() % (-1, -1)
+        query = geocode_replace_loc.foreign_full_name_1_sql() % (-1, -1)
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows, "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_full_name_2_sql(self):
-        query = foreign_full_name_2_sql() % (-1, -1)
+        query = geocode_replace_loc.foreign_full_name_2_sql() % (-1, -1)
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows, "{0} should be {1}".format(num_rows,-1))    
         pass
 
     def test_foreign_short_form_sql(self):
-        query = foreign_short_form_sql() % (-1, -1)
+        query = geocode_replace_loc.foreign_short_form_sql() % (-1, -1)
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows , "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_block_split_sql(self):
-        query = foreign_block_split_sql() % (-1, -1)
+        query = geocode_replace_loc.foreign_block_split_sql() % (-1, -1)
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows, "{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_first3_jaro_winkler_sql(self):
-        query = foreign_first3_jaro_winkler_sql() % (-1, -1, "20.92", -1)
+        query = geocode_replace_loc.foreign_first3_jaro_winkler_sql() % (-1, -1, "20.92", -1)
         result = self.cursor.execute(query)
         num_rows= result.rowcount
         self.assertEquals(-1, num_rows, "{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_last4_jaro_winkler_sql(self):
-        query = foreign_last4_jaro_winkler_sql() % (-1, -1, "20.90", -1)
+        query = geocode_replace_loc.foreign_last4_jaro_winkler_sql() % (-1, -1, "20.90", -1)
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_domestic_2nd_layer_sql(self):
-        query = domestic_2nd_layer_sql()
+        query = geocode_replace_loc.domestic_2nd_layer_sql()
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_domestic_first3_2nd_jaro_winkler_sql(self):
-        query = domestic_first3_2nd_jaro_winkler_sql() % "14.95"
+        query = geocode_replace_loc.domestic_first3_2nd_jaro_winkler_sql() % "14.95"
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_full_name_2nd_layer_sql(self):
-        query = foreign_full_name_2nd_layer_sql()
+        query = geocode_replace_loc.foreign_full_name_2nd_layer_sql()
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows, -1))
         pass
 
     def test_foreign_full_nd_2nd_layer_sql(self):
-        query = foreign_full_nd_2nd_layer_sql()
+        query = geocode_replace_loc.foreign_full_nd_2nd_layer_sql()
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_no_space_2nd_layer_sql(self):
-        query = foreign_no_space_2nd_layer_sql()
+        query = geocode_replace_loc.foreign_no_space_2nd_layer_sql()
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1,num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_foreign_first3_2nd_jaro_winkler_sql(self):
-        query = foreign_first3_2nd_jaro_winkler_sql() % "24.95"
+        query = geocode_replace_loc.foreign_first3_2nd_jaro_winkler_sql() % "24.95"
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
         pass
 
     def test_domestic_zipcode_sql(self):
-        query = domestic_zipcode_sql()
+        query = geocode_replace_loc.domestic_zipcode_sql()
         result = self.cursor.execute(query)
         num_rows = result.rowcount
         self.assertEquals(-1, num_rows,"{0} should be {1}".format(num_rows,-1))
