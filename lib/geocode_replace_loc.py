e517f5d3e2379e848962ed3ab3caa2b6036c921e
Kevin Johnson <vadskye@gmail.com>
2013-07-09 20:23:23 -0700

    Include uppercase to mirror old rev_wrd functionality

--- lib/geocode_replace_loc.py
+++ lib/geocode_replace_loc.py
@@ -194,7 +194,7 @@ def domestic_last4_jaro_winkler_sql():
                 b.longitude
           FROM  loc AS a
     INNER JOIN  usloc AS b
-            ON  SUBSTR(remove_spaces(GET_ENTRY_FROM_ROW(a.City, %d)),-4) = b.City4R
+            ON  UPPER(SUBSTR(remove_spaces(GET_ENTRY_FROM_ROW(a.City, %d)),-4)) = b.City4R
            AND  a.state = b.state
            AND  a.country = 'US'
          WHERE  jaro > %s
@@ -353,7 +353,7 @@ def foreign_last4_jaro_winkler_sql():
                 b.long
           FROM  loc AS a
     INNER JOIN  loctbl.gnsloc AS b
-            ON  SUBSTR(remove_spaces(GET_ENTRY_FROM_ROW(a.City, %d)),-4) = b.sort_name_ro
+            ON  UPPER(SUBSTR(remove_spaces(GET_ENTRY_FROM_ROW(a.City, %d)),-4)) = b.sort_name_ro
            AND  a.country = b.cc1
          WHERE  jaro > %s
            AND  separator_count(a.City) >= %d
