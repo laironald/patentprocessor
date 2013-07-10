39de0dff221e33e767a85c9def804a0fe43163da
Ron MacBook Air <laironald@gmail.com>
2013-07-10 12:42:33 -0500

    added testing file

---
+++ test/test_alchemy.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+sys.path.append('../lib/')
+from alchemy import *
+
+
+class TestAscit(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_general(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
