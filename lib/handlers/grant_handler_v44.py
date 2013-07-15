dd223d199a418853f8f2005e2a5dc376259e3124
Gabe Fierro <gtfierro225@gmail.com>
2013-07-15 12:42:38 -0700

    remove deprecated code from v44 parser

--- lib/handlers/grant_handler_v44.py
+++ lib/handlers/grant_handler_v44.py
@@ -47,15 +47,12 @@ class PatentGrant(object):
         self.abstract = xh.root.us_patent_grant.abstract.contents_of('p', '', as_string=True, upper=False)
         self.invention_title = self._invention_title()
 
-        # To depreciate >>>>>>
         self.asg_list = self._asg_list()
         self.cit_list = self._cit_list()
         self.rel_list = self._rel_list()
         self.inv_list = self._inv_list()
         self.law_list = self._law_list()
-        # <<<<<<
 
-        # To consolidate with above? >>>>>>
         self.pat = {
             "id": self.patent,
             "type": self.pat_type,
@@ -74,24 +71,12 @@ class PatentGrant(object):
             "country": self.country_app,
             "date": self._fix_date(self.date_app)
         }
-        # <<<<<<
 
     def _invention_title(self):
         original = self.xml.contents_of('invention_title', upper=False)[0]
         if isinstance(original, list):
             original = ''.join(original)
         return original
-
-    def _classes(self):
-        main = self.xml.classification_national.contents_of('main_classification')
-        further = self.xml.classification_national.contents_of('further_classification')
-        it = [main[0] if xml_util.has_content(main) else []]
-        if xml_util.has_content(further):
-            it.extend(further)
-        if not it or not it[0]:
-            return []
-        else:
-            return [[x[:3].replace(' ', ''), x[3:].replace(' ', '')] for x in it]
 
     def _name_helper(self, tag_root):
         """
@@ -131,133 +116,6 @@ class PatentGrant(object):
         except Exception as inst:
             print inst, datestring
             return None
-
-    # To depreciate >>>>>>
-    def _asg_list(self):
-        doc = self.xml.assignees.assignee
-        data = []
-        if not doc:
-            return []
-        if doc.first_name:
-            data = [1]
-            firstname, lastname = self._name_helper(doc)
-            data.extend(lastname)
-            data.extend(firstname)
-        else:
-            data = [0]
-            data.extend(doc.contents_of('orgname'))
-            data.extend(doc.contents_of('role'))
-        for tag in ['street', 'city', 'state', 'country', 'postcode']:
-            data.extend(doc.addressbook.address.contents_of(tag))
-        data.extend(doc.nationality.contents_of('country'))
-        data.extend(doc.residence.contents_of('country'))
-        return [data]
-
-    def _cit_list(self):
-        res = []
-        citations = self.xml.us_references_cited.us_citation
-        for citation in citations:
-            cit_data = citation.contents_of('category')
-            if citation.patcit:
-                for tag in ['country', 'doc_number', 'date', 'kind', 'name']:
-                    contents = citation.contents_of(tag)
-                    if isinstance(contents, list) and contents:
-                        cit_data.append(contents[0])
-                    else:
-                        cit_data.append(contents if xml_util.has_content(contents) else '')
-                cit_data.append('')
-            if citation.othercit:
-                contents = citation.contents_of('othercit')
-                for chunk in contents:
-                    cit_data.extend(['', '', '', '', ''])
-                    if isinstance(chunk, list):
-                        cit_data.append(''.join([xml_util.escape_html_nosub(x) for x in chunk]).upper())
-                    else:
-                        cit_data.append(xml_util.escape_html_nosub(chunk))
-            res.append(cit_data)
-        return res
-
-    def _rel_helper(self, base, roots, taglist):
-        """
-        Given a list of XMLElements as the [roots], look for each of the tags
-        in [taglist] and create a list of the contents of the tags for each of
-        the roots. Starts each of the content lists with [base]
-        """
-        res = []
-        for root in roots:
-            data = base
-            for tag in taglist:
-                contents = root.contents_of(tag, default=[''])
-                data.extend(contents[:1] if isinstance(contents, list) else [contents])
-            res.append(data)
-        return res
-
-    def _rel_list(self):
-        res = []
-        taglist = ['doc_number', 'country', 'kind']
-        for tag in ['continuation_in_part', 'continuation', 'division', 'reissue']:
-            main = self.xml.__getattr__(tag)
-            if not main:
-                continue
-            tag = tag.replace('_', '-').upper()
-            relations = main.relation  # get all relations
-            for relation in relations:
-                if relation.child_doc:
-                    res.extend(self._rel_helper([tag, -1], relation.child_doc, taglist))
-                base = [tag, 1]
-                taglist.extend(['date', 'parent_status'])
-                if relation.parent_doc:
-                    res.extend(self._rel_helper(base, relation.parent_doc, taglist))
-                if relation.parent_doc.parent_grant_document:
-                    res.extend(self._rel_helper(base, relation.parent_doc.parent_grant_document, taglist))
-                if relation.parent_doc.parent_pct_document:
-                    res.extend(self._rel_helper(base, relation.parent_doc.parent_pct_document, taglist))
-            if res:
-                break
-        for tag in ['related-publication', 'us-provisional-application']:
-            if not self.xml.__getattr__(tag):
-                continue
-            if self.xml.document_id:
-                tmp = [tag, 0]
-                for nested in ['doc_number', 'country', 'kind']:
-                    tmp.extend(self.xml.document_id.contents_of(nested))
-                res.append(tmp)
-            if res:
-                break
-        return res
-
-    def _inv_list(self):
-        inventors = self.xml.inventors.inventor
-        if not inventors:
-            return []
-        res = []
-        for inventor in inventors:
-            data = []
-            firstname, lastname = self._name_helper(inventor.addressbook)
-            data.append(lastname)
-            data.append(firstname)
-            for tag in ['street', 'city', 'state', 'country', 'postcode']:
-                data.append(inventor.addressbook.address.contents_of(tag, as_string=True))
-            data.append(inventor.nationality.contents_of('country', as_string=True))
-            data.append(inventor.residence.contents_of('country', as_string=True))
-            res.append(data)
-        return res
-
-    def _law_list(self):
-        lawyers = self.xml.agents.agent
-        if not lawyers:
-            return []
-        res = []
-        for lawyer in lawyers:
-            data = []
-            firstname, lastname = self._name_helper(lawyer)
-            data.append(lastname)
-            data.append(firstname)
-            data.append(lawyer.contents_of('country', as_string=True))
-            data.append(lawyer.contents_of('orgname', as_string=True))
-            res.append(data)
-        return res
-    # <<<<<<
 
     def assignee_list(self):
         """
@@ -435,7 +293,6 @@ class PatentGrant(object):
           relationship
           sequence
         """
-        # TODO: look at PatentGrantXMLv44, page 30 and onward and figure out the best way to parse this
         root = self.xml.us_related_documents
         if not root:
             return []
