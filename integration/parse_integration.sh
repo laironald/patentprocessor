34ba29099a805dd8005c03a7dd722a554f3c6a2a
Gabe Fierro <gtfierro225@gmail.com>
2013-07-15 10:38:00 -0700

    incremental fixes to parse_integration to work with new csv files

--- integration/parse_integration.sh
+++ integration/parse_integration.sh
@@ -6,24 +6,31 @@
 
 cd ..
 
+echo 'Testing test/fixtures/xml/ipg120327.one.xml'
 make spotless > /dev/null
 mkdir -p tmp/integration/ipg120327.one
 ./parse.py -p test/fixtures/xml/ -x ipg120327.one.xml -o .
 
-for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+for table in application citation ipcr mainclass otherreference patent rawassignee rawinventor rawlawyer rawlocation subclass uspc usreldoc
 do
-  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.one/${table}.csv
+  echo $table 'diffs...'
+  sqlite3 -csv alchemy.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.one/${table}.csv
+  # remove UUIDs from database dump because these change each time
+  perl -pi -e 's/^[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12},//' tmp/integration/ipg120327.one/${table}.csv
   diff test/integration/parse/ipg120327.one/${table}.csv tmp/integration/ipg120327.one/${table}.csv
 done
 
-# TODO: Refactor
+echo 'Testing test/fixtures/xml/ipg120327.two.xml'
 make spotless > /dev/null
 mkdir -p tmp/integration/ipg120327.two
 ./parse.py -p test/fixtures/xml/ -x ipg120327.two.xml -o .
 
-for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+for table in application citation ipcr mainclass otherreference patent rawassignee rawinventor rawlawyer rawlocation subclass uspc usreldoc
 do
-  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.two/${table}.csv
+  echo $table 'diffs...'
+  sqlite3 -csv alchemy.sqlite3 "select * from ${table}"  > tmp/integration/ipg120327.two/${table}.csv
+  # remove UUIDs from database dump because these change each time
+  perl -pi -e 's/^[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12},//' tmp/integration/ipg120327.two/${table}.csv
   diff test/integration/parse/ipg120327.two/${table}.csv tmp/integration/ipg120327.two/${table}.csv
 done
 
