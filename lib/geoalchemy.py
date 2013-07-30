cbed222efd0b1917bbb2b7018318afd9c1d5d623
Ron MacBook Air <laironald@gmail.com>
2013-07-30 00:50:58 -0500

    added timing blocks

--- lib/geoalchemy.py
+++ lib/geoalchemy.py
@@ -4,6 +4,7 @@ import sqlalchemy.ext.declarative as declarative
 import geoalchemy_util
 import itertools
 import os
+import datetime
 
 import alchemy
 config = alchemy.get_config()
@@ -40,6 +41,7 @@ raw_google_session = raw_google_session_class()
 
 
 def main():
+    t = datetime.datetime.now()
     #Get all of the raw locations from the XML parsing
     raw_parsed_locations = alchemy.session.query(alchemy.RawLocation)
     #raw_google_locations = raw_google_session.query(RawGoogle)
@@ -60,14 +62,14 @@ def main():
             grouped_locations.append({"raw_location": instance,
                                       "matching_location": matching_location,
                                       "grouping_id": grouping_id})
-    print "grouped_locations created"
+    print "grouped_locations created", datetime.datetime.now() - t
     #We now have a list of all locations in the file, along with their
     #matching locations and the id used to group them
     #Sort the list by the grouping_id
     keyfunc = lambda x: x['grouping_id']
     grouped_locations.sort(key=keyfunc)
-    print "grouped_locations sorted"
-    #Group by the grouping_id
+    print "grouped_locations sorted", datetime.datetime.now() - t
+    #Group by the grouping_id    
     for key, grouping in itertools.groupby(grouped_locations, keyfunc):
         #match_group is the list of RawLocation objects which we call match on
         #We need to get only the RawLocation objects back from the dict
@@ -83,8 +85,7 @@ def main():
             longitude = 0
         for grouped_location in grouping:
             match_group.append(grouped_location["raw_location"])
-
         alchemy.match(match_group, alchemy.session, {"latitude": latitude, "longitude": longitude})
-        #print "match called"
-    print "Matches made!"
+
+    print "Matches made!", datetime.datetime.now() - t
     print "%s groups formed from %s locations" % (len(grouped_locations), raw_parsed_locations.count())
