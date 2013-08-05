e1063c4872cdca3b0cded3ae922c748393cc1124
Kevin Johnson <vadskye@gmail.com>
2013-08-04 20:33:42 -0700

    Revised accent parsing

--- lib/geoalchemy_util.py
+++ lib/geoalchemy_util.py
@@ -25,9 +25,9 @@ def generate_manual_patterns_and_replacements():
     replacement_count=0
     for line in manual_replacement_file:
         #allow # to be a comment
-        if(line[0]=='#'):
+        if(line[0]=='#' or line=='\n'):
             continue
-        line_without_newline = re.sub('[\r\n]','',line)
+        line_without_newline = re.sub('[\r\n]+','',line)
         line_split = line_without_newline.split("|")
         #An individual re can only hold 100 entities. So split if necessary.
         if(replacement_count<99):
@@ -77,18 +77,20 @@ manual_dict = generate_manual_patterns_and_replacements()
 manual_patterns = manual_dict['patterns']
 manual_replacements = manual_dict['replacements']
 quickfix_patterns = generate_quickfix_patterns()
-postal_pattern = re.compile(ur'(- )?[A-Z\-#\(]*\d+[\)A-Z]*')
-foreign_postal_pattern = re.compile(ur'[A-Z\d]{3,4}[ ]?[A-Z\d]{3}')
-england_pattern = re.compile(ur', EN')
-germany_pattern = re.compile(ur', DT')
+postal_pattern = re.compile(ur'(- )?[A-Z\-#\(]*\d+[\)A-Z]*', re.UNICODE)
+foreign_postal_pattern = re.compile(ur'[A-Z\d]{3,4}[ ]?[A-Z\d]{3}', re.UNICODE)
+england_pattern = re.compile(ur', EN', re.UNICODE)
+germany_pattern = re.compile(ur', DT', re.UNICODE)
 
-unnecessary_symbols_pattern = re.compile(ur'[#\(?<!.\)]')
-excess_whitespace = re.compile(ur'[ ]+')
-whitespace_around_commas_pattern = re.compile(ur'[ ]*,+[ ]*,*[ ]*')
-start_of_line_pattern = re.compile(ur'^[-,]+[ ]*', re.MULTILINE)
+separator_pattern = re.compile(ur'\|', re.UNICODE)
+unnecessary_symbols_pattern = re.compile(ur'[#\(?<!.\)]', re.UNICODE)
+excess_whitespace = re.compile(ur' [ ]+', re.UNICODE)
+whitespace_around_commas_pattern = re.compile(ur'(( )*,( )*)+', re.UNICODE)
+start_of_line_pattern = re.compile(ur'^[-,]*[ ]*', re.MULTILINE)
 
 #Input: a raw location from the parse of the patent data
 def clean_raw_location(text):
+    text = separator_pattern.sub(', ', text)
     #Perform all of the manual replacements
     i=0
     while i<len(manual_patterns):
@@ -97,20 +99,19 @@ def clean_raw_location(text):
     #Perform all the quickfix replacements
     text = quickfix_patterns['curly'].sub(get_chars_in_parentheses, text)
     
-    text = foreign_postal_pattern.sub('', text)
-    text = postal_pattern.sub('', text)
+    #Turn accents into unicode
+    soup = BeautifulSoup(text)
+    text = unicode(soup.get_text())
+    text =  unicodedata.normalize('NFC', text)
     
     text = unnecessary_symbols_pattern.sub('', text)
-    #around commas
-    text = whitespace_around_commas_pattern.sub(', ', text)
+    text = foreign_postal_pattern.sub('', text)
+    text = postal_pattern.sub('', text)
     #Remove start-of-line [-,] and extra whitespace
     text = start_of_line_pattern.sub('', text)
     text = excess_whitespace.sub(' ', text)
+    #around commas
+    text = whitespace_around_commas_pattern.sub(', ', text)
     text = england_pattern.sub(', GB', text)
     text = germany_pattern.sub(', DE', text)
-    
-    #Turn accents into unicode
-    soup = BeautifulSoup(text)
-    souptext = unicode(soup.get_text())
-    souptext =  unicodedata.normalize('NFC', souptext)
-    return souptext
+    return text
