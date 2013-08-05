7a53b69fa8c5bd62adc956641237b1ecf0683135
Kevin Johnson <vadskye@gmail.com>
2013-08-05 13:40:43 -0700

    Remerged - didn't work properly before

--- clean.py
+++ clean.py
@@ -3,26 +3,7 @@
 from lib import assignee_disambiguation
 from lib import lawyer_disambiguation
 from lib import geoalchemy
-<<<<<<< HEAD
-import datetime
 
-t1 = datetime.datetime.now()
-
-||||||| merged common ancestors
-import datetime
-
-t1 = datetime.datetime.now()
-
-## run geocoding
-#print "START: geocode", t1
-#import lib.geocode
-##print "   - Loc Merge", "\n   -", datetime.datetime.now()-t1
-#print"DONE: geocode"
-#print "   -", datetime.datetime.now()-t1
-
-=======
-
->>>>>>> c740e03e1b1ddef82669973da6412b862b56aa88
 # run assignee disambiguation and populate the Assignee table
 assignee_disambiguation.run_disambiguation()
 
